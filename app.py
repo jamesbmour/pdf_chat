c17a80e51ffc55ab3287c2223d0cd0e90e72f53b
James <jamesbrendamour7@yahoo.com>
2023-08-09 06:38:06 -0600

    08/09/2023

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+# from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings #, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
-import InstructorEmbedding
+# from langchain.llms import HuggingFaceHub
+# import InstructorEmbedding
 # load environment variables
 load_dotenv()
 # get pdf text method
