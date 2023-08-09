4799e0ff3d47dfd1854be4f507035c2102a52668
James <jamesbrendamour7@yahoo.com>
2023-08-09 06:46:49 -0600

    added pic of bryn

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://th.bing.com/th/id/OIP.DP2q-HPIzuLiVN_NbFIwKQHaHb?pid=ImgDet&rs=1">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
